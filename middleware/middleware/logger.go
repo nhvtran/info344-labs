3d9176c5dbb5e140945e13d8fe8609c7d8838600
Nancy Tran <nhvt@uw.edu>
2017-10-26 15:31:16 -0700

    Add the code from the lecture part of the lab

--- middleware/middleware/logger.go
+++ middleware/middleware/logger.go
@@ -1,3 +1,40 @@
 package middleware
 
 //TODO: implement a Logger middleware handler
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(statusCode int) {
+	lrw.statusCode = statusCode
+	lrw.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (lrw *loggingResponseWriter) Write(data []byte) (int, error) {
+	log.Println("writing something...")
+	return lrw.ResponseWriter.Write(data)
+}
+
+type Logger struct {
+	handler http.Handler
+}
+
+func NewLogger(handler http.Handler) *Logger {
+	return &Logger{handler: handler}
+}
+
+func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	lrw := &loggingResponseWriter{w, http.StatusOK}
+	start := time.Now()
+	l.handler.ServeHTTP(lrw, r)
+
+	// using log instead of fmt because it will print the time stamp of when this was printed
+	log.Printf("%s %s %d %v", r.Method, r.URL.Path, lrw.statusCode, time.Since(start))
+}
